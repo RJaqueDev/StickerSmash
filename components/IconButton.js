ff8fedf605abcc1254aa57d3374e1b6fd288eaaf
Raul Marcelo Jaque Astudillo <Raul.Jaque@dimerc.cl>
2024-10-01 15:00:19 -0300

    Base de la aplicación

---
+++ components/IconButton.js
@@ -0,0 +1,10 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+import  MaterialIcons from "@expo/vector-icons/MaterialIconsl";
+
+export default function IconButton({icon, label, onPress}){
+    return(
+        <View>
+            
+        </View>
+    )
+}
